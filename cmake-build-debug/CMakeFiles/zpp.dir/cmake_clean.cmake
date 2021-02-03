9ad9e8788464f6fe6d717f9b166f0df89fcb9dd7
Hongyi <hongyi@163.com>
2021-02-02 22:52:27 -0800

    test cpp hello world

---
+++ cmake-build-debug/CMakeFiles/zpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zpp.dir/main.cpp.o"
+  "libzpp.dll.a"
+  "zpp.exe"
+  "zpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
